46fcbf7e41633a77571397e1bedf48a5ddcc93ca
Kevin Horecka <kevin.horecka@gmail.com>
2017-05-02 12:58:22 -0500

    Update Install.bat
    Added Video Logger dependencies.

--- Install.bat
+++ Install.bat
@@ -4,6 +4,7 @@ START /WAIT powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).
 echo Install Anaconda Python...
 START /WAIT Anaconda2-4.2.0-Windows-x86_64.exe /S
 echo Installing Additional Packages...
-pip install plotly argparse
+python -m pip install plotly argparse hachoir_core hachoir_parser hachoir_metadata pygame
+python -m conda install --channel https://conda.anaconda.org/menpo opencv3
 echo Done. You may close this window at any time.
 pause
